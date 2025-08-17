8a33b73b09cd06c6edf1c61a5d41025ce611745a
Jesse Collins <jessecollins0808@gmail.com>
2025-08-17 13:47:53 +0100

    Template version 2

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,37 +1,10 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    static: "./dist",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+});
